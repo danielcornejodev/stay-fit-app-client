779950b5f8872a29f0d7a893e4414203fea2f840
Daniel Cornejo <contact@danielcornejo.dev>
2023-08-02 19:12:23 -0400

    read workout details

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='/workouts/create' element={<IsPrivate> <CreateWorkoutsPage /> </IsPrivate>} />
           <Route path='/workouts' element={<IsPrivate> <WorkoutsPage /> </IsPrivate>} />
+          <Route path='/workouts/:id' element={<IsPrivate> <WorkoutDetails /> </IsPrivate>} />
 
           <Route path="/signup" element={<IsAnon> <SignupPage /> </IsAnon> } />
           <Route path="/login" element={ <LoginPage /> } />
--- src/pages/Workouts/WorkoutDetails.js
+++ src/pages/Workouts/WorkoutDetails.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { React, useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom'; // Import useParams to get the workout id from the URL params
+import WorkoutsService from '../../services/workouts.service';
 
 export default function WorkoutDetails() {
+
+  const { id } = useParams(); 
+
+    // Here, you can use the workout id to fetch the specific workout details using Axios or any other method
+  // For example:
+  const [workoutDetails, setWorkoutDetails] = useState(null);
+
+  useEffect(() => {
+    WorkoutsService.getWorkout()
+      .then((res) => setWorkoutDetails(res.data))
+      .catch((error) => console.log(error));
+  }, [id]);
+
+  console.log('workout details:', workoutDetails )
+
   return (
-    <Link>Create New Exercise</Link>
+    <div>
+      <h2>Workout Details</h2>
+      <p>Workout ID: {id}</p>
+    </div>
   )
 }
